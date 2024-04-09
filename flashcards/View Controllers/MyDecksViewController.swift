b52971ef3d5a6ee1520926eb4057d1981424dd4e
danuja01 <it21023378@my.sliit.lk>
2024-04-09 15:12:23 +0530

    Initial commit

---
+++ flashcards/View Controllers/MyDecksViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  flashcards
+//
+//  Created by Danuja Jayasuriya on 2024-04-09.
+//
+
+import UIKit
+
+class MyDecksViewController: UIViewController {
+
+    @IBOutlet var cardView: CustomView!
+    @IBOutlet var blurView: UIVisualEffectView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
